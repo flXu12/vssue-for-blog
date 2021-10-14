0b1721b4806c767deca2624c53ab5b7b127cdef6
xufanglin <xufanglin@qianxin.com>
2021-10-15 00:01:26 +0800

    feat: 配置unplugin-vue-components按需引入ant design vue

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,8 +1,27 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>页面布局</div>
+  <a-layout>
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout></a-layout>
+  </a-layout>
   <router-view></router-view>
 </template>
+
+<script setup lang="ts"></script>
 
 <style></style>
